7b8e311664679abd54b17a6cf8c044611a01bfe3
Cielle Carlton <cielle.carlton13@gmail.com>
2021-01-07 21:07:40 -0500

    fixed the routes

---
+++ routes/api.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
+router.put("/api/workouts/:id", ({body,params}, res) => {
+    console.log(body);
+    console.log(params.id);
+    Workout.findByIdAndUpdate(params.id, {
+            $push: {
+                exercises: body
+            }
+        })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({}).limit(7).then(dbWorkout => {
+        console.log(dbWorkout)
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    })
+})
+
+router.delete("/api/workouts", ({
+    body
+}, res) => {
+    Workout.findByIdAndDelete(body.id).then(() => {
+        res.json(true)
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,101 +1,144 @@
-//===IMPORT NPM PACKAGES====================================
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const db = require('./models');
-const { on } = require('process');
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const mongojs = require("mongojs");
+const path = require("path")
+const db = require("./models");
+
 const app = express();
 
-//===SETTING UP THE PORT=================================
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
-//===STATIC DIRECTORY===================================
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("dev"));
 
-//===DATA PARSING===========================================
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "workouts_db";
+const collections = ["workouts"];
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true , useFindAndModify : false});
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+// Routes
+app.use(require("./routes/api"))
+app.use(require("./routes/view"))
 
 
-//===HTML ROUTES======================================
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'))
-})
-
-app.get('/exercise', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/exercise.html'))
-})
-
-app.get('/stats', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/stats.html'))
-})
-
-app.post("/api/workouts", (req, res) => {
-  db.Workout.create(req.body)
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    })
-})
-
-//===API ROUTES=================================================
-app.put("/api/workouts/:id", ({ body, params }, res) => {
-  console.log(body);
-  console.log(params.id);
-  db.Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}})
-  .then(dbWorkouts => {
-    res.json(dbWorkouts);
+app.listen(process.env.PORT || 3000, () => {
+    console.log("app is listening" + PORT);
   })
-  .catch(err => {
-    res.status(400).json(err);
-  })
-});
 
-app.get("/api/workouts", (req, res) => {
-  db.Workout.find({})
-  .then(dbWorkouts => {
-    res.json(dbWorkouts);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  })
-});
+  
+  // //===IMPORT NPM PACKAGES====================================
+// const express = require('express');
+// const path = require('path');
+// const mongoose = require('mongoose');
+// const db = require('./models');
+// const { on } = require('process');
+// const app = express();
 
-app.get("/api/workouts/range", (req, res) => {
-  db.Workout.find({})
-  .then(dbWorkouts => {
-    res.json(dbWorkouts);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  })
-});
+// //===SETTING UP THE PORT=================================
+// const PORT = process.env.PORT || 3000;
 
-app.delete("/api/workouts", ({ body }, res) => {
+// //===STATIC DIRECTORY===================================
+// app.use(express.static(path.join(__dirname, 'public')))
 
-})
+// //===DATA PARSING===========================================
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
 
 
+// //===HTML ROUTES======================================
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/public/index.html'))
+// })
+
+// app.get('/exercise', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/public/exercise.html'))
+// })
+
+// app.get('/stats', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/public/stats.html'))
+// })
+
+// app.post("/api/workouts", (req, res) => {
+//   db.Workout.create(req.body)
+//     .then(dbWorkouts => {
+//       res.json(dbWorkouts);
+//     })
+//     .catch(err => {
+//       res.status(400).json(err);
+//     })
+// })
+
+// //===API ROUTES=================================================
+// app.put("/api/workouts/:id", ({ body, params }, res) => {
+//   console.log(body);
+//   console.log(params.id);
+//   db.Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}})
+//   .then(dbWorkouts => {
+//     res.json(dbWorkouts);
+//   })
+//   .catch(err => {
+//     res.status(400).json(err);
+//   })
+// });
+
+// app.get("/api/workouts", (req, res) => {
+//   db.Workout.find({})
+//   .then(dbWorkouts => {
+//     res.json(dbWorkouts);
+//   })
+//   .catch(err => {
+//     res.status(400).json(err);
+//   })
+// });
+
+// app.get("/api/workouts/range", (req, res) => {
+//   db.Workout.find({})
+//   .then(dbWorkouts => {
+//     res.json(dbWorkouts);
+//   })
+//   .catch(err => {
+//     res.status(400).json(err);
+//   })
+// });
+
+// app.delete("/api/workouts", ({ body }, res) => {
+
+// })
 
 
-//===CONNECTING TO MONGODB======================================
-mongoose.connect('mongodb://localhost/workout', 
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
-mongoose.connection.once('open', function(){
-  console.log('Connected to the database!');
-}).on('error', function(error){
-  console.log('Connection error:', error)
-})
+
+
+// //===CONNECTING TO MONGODB======================================
+// mongoose.connect('mongodb://localhost/workout', 
+// {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// })
+// mongoose.connection.once('open', function(){
+//   console.log('Connected to the database!');
+// }).on('error', function(error){
+//   console.log('Connection error:', error)
+// })
  
   
-//===STARTING OUR EXPRESS APP===============================
-app.listen(PORT, () =>{
-  console.log(`App listening on http://localhost:${PORT}`)
-})
+// //===STARTING OUR EXPRESS APP===============================
+// app.listen(PORT, () =>{
+//   console.log(`App listening on http://localhost:${PORT}`)
+// })
